2167e65d1cc026001c6e90caad0bac26877198a0
Ярослав <@DESKTOP-9DD6V4F>
2019-05-08 14:56:22 +0300

    new

--- udav_poehavshii.py
+++ udav_poehavshii.py
@@ -5,8 +5,9 @@ x1 = 0
 x2 = 0
 x3 = 0 
 
-while 1:
-    sc = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+sc = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+while True:
     sc.fill((x1,x2,x3))
     if x1<255:
         x1+=1
@@ -16,6 +17,7 @@ while 1:
     if x2 == 255:
         if x3 < 255:
             x3+=1   
-    pygame.time.delay(10)
+    pygame.time.delay(10) 
     pygame.display.update()
+   
    
