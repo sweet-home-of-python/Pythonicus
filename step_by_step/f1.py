f46900e2995e0f8b53ca760ed164823ac0343842
aid219 <aid219@DESKTOP-H9BSPJU>
2019-05-10 12:22:22 +0300

    polet

--- step_by_step/f1.py
+++ step_by_step/f1.py
@@ -1,5 +1,5 @@
 import pygame
- 
+import time 
 FPS = 60
 W = 700  # ширина экрана
 H = 300  # высота экрана
@@ -9,13 +9,50 @@ black = (0, 0, 0)
 pygame.init()
 sc = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
- 
+
+
+  
+
+   
 # координаты и радиус круга
 x = W // 2
 y = H // 2
 r = 50
- 
-while 1:
+Play = True 
+def polet(z,vec,x,y):
+    if vec == 6:
+        while x < z:
+            pygame.time.delay(5)
+            sc.fill(WHITE)
+            x += 1
+            pygame.draw.circle(sc, black, (x, y), r) 
+            pygame.display.update()
+    if vec == 4:
+        while x > z:
+            pygame.time.delay(5)
+            sc.fill(WHITE)
+            x -= 1
+            pygame.draw.circle(sc, black, (x, y), r) 
+            pygame.display.update()
+        
+    if vec == 8:
+        while y > z:
+            pygame.time.delay(5)
+            sc.fill(WHITE)
+            y -= 1
+            pygame.draw.circle(sc, black, (x, y), r) 
+            pygame.display.update()
+                  
+    if vec == 2:
+        while y < z:
+            pygame.time.delay(5)
+            sc.fill(WHITE)
+            y += 1
+            pygame.draw.circle(sc, black, (x, y), r) 
+            pygame.display.update()
+    return x,y
+     
+while Play:
     sc.fill(WHITE)
  
     pygame.draw.circle(sc, black, (x, y), r)
@@ -24,7 +61,7 @@ while 1:
  
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
-            exit()
+            Play = False
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_LEFT:
                 x -= 3
@@ -34,4 +71,12 @@ while 1:
                 y -= 3
             elif i.key == pygame.K_DOWN:
                 y += 3
+            elif i.key == pygame.K_d:
+                x,y = polet(x + 200, 6,x, y)
+            elif i.key == pygame.K_s:
+                x,y = polet(y + 200, 2,x, y)
+            elif i.key == pygame.K_a:
+                x,y = polet(x - 200, 4,x, y)
+            elif i.key == pygame.K_w:
+                x,y = polet(y - 200, 8,x, y)        
     clock.tick(FPS)
