c0b25913a1b90dda6ca7a0b1085866542e450e7d
Ярослав <@DESKTOP-9DD6V4F>
2019-05-13 13:38:54 +0300

    temp

---
+++ step_by_step/test_field.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+import gameData
+
+FPS = 60
+W = 1200  # ширина экрана
+H = 700  # высота экрана
+pygame.init()
+sc = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
+
+
+pygame.display.update()
