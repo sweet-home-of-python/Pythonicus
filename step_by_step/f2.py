182d1e64f82473ae247906652c00f03e498d260e
Ярослав <@DESKTOP-9DD6V4F>
2019-05-13 15:50:40 +0300

    temp

--- step_by_step/f2.py
+++ step_by_step/f2.py
@@ -1,34 +1,7 @@
 import pygame
 import time 
 
-
-from pygame import draw
-from random import randint
-class staticObjects(object):
-    def __init__(self,name):
-        self.name = ''
-        self.pos = (0,0,0,0)
-        self.type = 'rect'
-        #self.params = [color,opacity]
-        self.colors = {'black': (0,0,0),
-                       'white':(255,255,255),
-                       'red':(255,0,0),
-                       'green':(0,255,0),
-                       'blue':(0,0,255)
-                       }
-        self.types = ('rect',
-                      'circle'
-                     )
-
-
-    def random_rect(self):
-        self.type = 'rect'
-        self.pos = (randint(0,1000),randint(0,600), randint(0,500), randint(0,500)) 
-
-    def drow(self,screen):
-        if self.type == 'rect':
-            draw.rect(screen, self.colors['black'], self.pos)
-
+from gameData import *
 
 
 FPS = 60
--- step_by_step/gameData.py
+++ step_by_step/gameData.py
@@ -1,19 +1,26 @@
 from pygame import draw
 from random import randint
 class staticObjects(object):
-    def __init__(self,name,*params):
+    def __init__(self,name):
         self.name = ''
         self.pos = (0,0,0,0)
-        self.params = [color,opacity]
+        self.type = 'rect'
+        #self.params = [color,opacity]
         self.colors = {'black': (0,0,0),
                        'white':(255,255,255),
                        'red':(255,0,0),
                        'green':(0,255,0),
                        'blue':(0,0,255)
                        }
+        self.types = ('rect',
+                      'circle'
+                     )
 
 
-    def random_rect(self,screen):
-        self.pos = (randint(0,1000),randint(0,1000), randint(0,100), randint(0,100))
-        draw.rect(screen, self.colors['black'], self.pos)
-        return self.pos
+    def random_rect(self):
+        self.type = 'rect'
+        self.pos = (randint(0,1000),randint(0,600), randint(0,500), randint(0,500)) 
+
+    def drow(self,screen):
+        if self.type == 'rect':
+            draw.rect(screen, self.colors['black'], self.pos)
