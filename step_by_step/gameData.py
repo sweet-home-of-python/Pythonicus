437052d3beb18ecc71311aff2d41941d82c1fd5c
Ярослав <@DESKTOP-9DD6V4F>
2019-05-15 11:40:32 +0300

    123

--- step_by_step/gameData.py
+++ step_by_step/gameData.py
@@ -1,4 +1,4 @@
-# GameData Версия 0.5
+# GameData Версия 0.67
 # 
 #
 #
@@ -9,64 +9,80 @@
 from pygame import draw
 from random import randint
 
-# Глобальные переменные
-
-colors = {'black': (0,0,0),
-          'white':(255,255,255),
-          'red':(255,0,0),
-          'green':(0,255,0),
-          'blue':(0,0,255)
-         }
-
-
-
-
+class Color:
+    '''Работает с RGB цветами'''
+    def __init__(self):
+        self.colors = {'black': (0,0,0),
+                        'white':(255,255,255),
+                        'red':(255,0,0),
+                        'green':(0,255,0),
+                        'blue':(0,0,255)
+                      }
+    def random_color():
+        return randint(0,256),randint(0,256),randint(0,256)
 
 
 class GameObject:
     objects = {}
  
+class Person:
+    def __init__(self):
+        self.pos = (0,0)
+        self.rad = 50
+        #self.screen = screen
+        self.prestep = self.pos
+    
+    def random_pos(self):
+        self.pos = (randint(0,1000),randint(0,500))
+        self.prestep = self.pos
+    
+    def draw(self):
+        pygame.draw.circle(self.screen, Color.random_color(), (self.pos), self.rad)
 
-
-class StaticObject(GameObject):
-      
+class StaticObject:
     def __init__(self,desc = 'static object'):
-  
+        # Инициализация
         self.class_name = 'staticObject' + str(len(self.objects))
         self.class_tag = 'staticObject'
         self.desc = desc
                           
         self.pos = (0,0,0,0)
-        self.type = 'rect'
-        #self.params = [color,opacity]
-        
-
-        self.types = ('rect',
-                      'circle')
 
         self.random_rect()
-
+        
         GameObject.objects[self.class_name] = self
        
 
     def random_rect(self):
-        self.type = 'rect'
         self.pos = (randint(0,1000),randint(0,600), randint(0,500), randint(0,500)) 
 
     def draw(self,screen):
-        if self.type == 'rect':
-            draw.rect(screen, colors['black'], self.pos)
+        draw.rect(screen, Color.colors['black'], self.pos)
+
+class Settings:
+    '''Настройки игры. Сюда входят параметры экрана и управления'''
+    def __init__(self):
+        #fullscreen window
+        self.mode = 'fullscreen'
+        self.fps = 60
+
+    def init_screen(self):
+        W = 1200  # ширина экрана
+        H = 700  # высота экрана
+        if self.mode == 'fullscreen':
+            return pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 
-def processingCollision(coords):
-    x,y = coords
-    for objkey in GameObject.objects:
-        x1,y1,x2,y2 = GameObject.objects[objkey].pos
-        if GameObject.objects[objkey].class_tag == 'staticObject':
-             if (x in range(x1-50,x1+x2+50) 
-                 and y in range(y1-50,y1+y2+50) 
-                 or x in range(x1-50,x1+x2+50) 
-                 and y in range(y1-50,y1+y2+50)) : #обработка столкновения
-                 return True
-             #else: return False
+class Collision:
+    def processingCollision(coords):
+        x,y = coords
+        for objkey in GameObject.objects:
+            x1,y1,x2,y2 = GameObject.objects[objkey].pos
+            if GameObject.objects[objkey].class_tag == 'staticObject':
+                 if (x in range(x1-50,x1+x2+50) 
+                     and y in range(y1-50,y1+y2+50) 
+                     or x in range(x1-50,x1+x2+50) 
+                     and y in range(y1-50,y1+y2+50)) : #обработка столкновения
+                     return True
+                 #else: return False
 
--- step_by_step/main.py
+++ step_by_step/main.py
@@ -1,34 +1,25 @@
 import pygame
 import time 
 from gameData import *
-FPS = 60
-W = 1200  # ширина экрана
-H = 700  # высота экрана
-WHITE = (255, 255, 255)
-black = (0, 0, 0)
-some = (100, 50, 200)
-STOP = "stop" 
-RIGHT = "to the right"
-LEFT = "to the left"
-UP = "up"
-DOWN = "down"
-D_R = "dr"
-D_L ="dl"
-U_R ="ur"
-U_L ="ul"
-prestep_x = W // 2
-prestep_y = H // 2
+
+
 pygame.init()
+Color = Color()
+Person = Person()
+Collision = Collision()
+
+
+
 sc = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
-x = W // 2
-y = H // 2
-r = 50
+
+#prestep_x,prestep_y = Person.prestep
+
 Play = True 
-motion = STOP
+motion = 'STOP'
 while Play:
-    sc.fill(WHITE)
-    pygame.draw.circle(sc, some, (x, y), r)
+    sc.fill(Color.colors['white'])
+    pygame.draw.circle(sc, Color.colors['black'], (Person.pos), Person.rad)
     for obj_tag in GameObject.objects:
         GameObject.objects[obj_tag].draw(sc)
     pygame.display.update()
@@ -40,21 +31,21 @@ while Play:
             Play = False
             pygame.quit()
         if keys[pygame.K_LEFT]:
-            motion = LEFT
+            motion = 'LEFT'
         if keys[pygame.K_RIGHT]:
-            motion = RIGHT
+            motion = 'RIGHT'
         if keys[pygame.K_UP]:
-            motion = UP
+            motion = 'UP'
         if keys[pygame.K_DOWN]:
-            motion = DOWN
+            motion = 'DOWN'
         if keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
-            motion = D_R
+            motion = 'D_R'
         if keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
-            motion = D_L
+            motion = 'D_L'
         if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
-            motion = U_R
+            motion = 'U_R'
         if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
-            motion = U_L
+            motion = 'U_L'
         if keys[pygame.K_y]:
             StaticObject()
         if keys[pygame.K_r]:
@@ -64,40 +55,40 @@ while Play:
              GameObject.objects = {}
         elif i.type == pygame.KEYUP:
             if i.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
-                motion = STOP
-    if processingCollision((x,y)):
+                motion = 'STOP'
+    if Collision.processingCollision((x,y)):
         x=prestep_x
         y=prestep_y
-    if motion == LEFT :
+    if motion == 'LEFT':
         prestep_x = x
         x -= 5
-    if motion == RIGHT :
+    if motion == 'RIGHT':
         prestep_x = x
         x += 5
-    if motion == UP :
+    if motion == 'UP':
        prestep_y = y
        y -= 5
-    if motion == DOWN :
+    if motion == 'DOWN':
         prestep_y = y
         y += 5
-    if motion == D_R :
+    if motion == 'D_R':
         prestep_x = x
         prestep_y = y
         y += 5
         x += 5
-    if motion == D_L :
+    if motion == 'D_L':
         prestep_x = x
         prestep_y = y
         y += 5
         x -= 5
-    if motion == U_R :
+    if motion == 'U_R':
         prestep_x = x
         prestep_y = y
         y -= 5
         x += 5
-    if motion == U_L :
+    if motion == 'U_L':
         prestep_x = x
         prestep_y = y
         y -= 5
         x -= 5
-    clock.tick(FPS)
+    clock.tick(Settings.fps)
