8ce7c9a2206b2082308e2a1b03ab2a2e6656ebb8
aid219 <aid219@DESKTOP-H9BSPJU>
2019-05-15 14:27:03 +0300

    konchennii_anal

--- step_by_step/udav_tolkaet_stol.py
+++ step_by_step/udav_tolkaet_stol.py
@@ -34,27 +34,31 @@ while Play:
         elif i.type == pygame.KEYUP:
             if i.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
                 motion = STOP
+    
     if (x in range(x1-r,x1+x2+r +1) 
                  and y in range(y1-r+20,y1+y2+r-20) 
                  or x in range(x1-r+20,x1+x2+r-20) 
                  and y in range(y1-r,y1+y2+r+1)):
-       if motion == LEFT :
+        if r < 50:
+            a =1
+        
+        if motion == LEFT :
            if y+r != y1 and y-r != y1+y2:
                 if prestep_x > x:
                     x1-=int(r/50*5)
-       if motion == RIGHT:
+        if motion == RIGHT:
            if y+r != y1 and y-r != y1+y2:
              if prestep_x < x:
                 x1+=int(r/50*5)
-       if motion == UP:
+        if motion == UP:
            if x+r != x1 and x-r != x1+x2:
                if prestep_y > y:
                  y1-=int(r/50*5)
-       if motion == DOWN:
+        if motion == DOWN:
            if x+r != x1 and x-r != x1+x2:
                if prestep_y < y:
                 y1+=int(r/50*5)
-       if motion == D_R:
+        if motion == D_R:
             if x<x1:
                if  prestep_y < y:
                    if y+r != y1 and y-r != y1+y2:
@@ -65,7 +69,7 @@ while Play:
                    if y+r != y1 and y-r != y1+y2:
                      if prestep_x < x:
                         y1+=int(r/50*5)
-       if motion == D_L:
+        if motion == D_L:
            if x>x1+x2:
                if  prestep_y < y:
                     if y+r != y1 and y-r != y1+y2:
@@ -76,7 +80,7 @@ while Play:
                    if y+r != y1 and y-r != y1+y2:
                      if prestep_x > x:
                         y1+=int(r/50*5)
-       if motion == U_R:
+        if motion == U_R:
            if x<x1:
                if  prestep_y > y:
                    if y+r != y1 and y-r != y1+y2:
@@ -87,7 +91,7 @@ while Play:
                    if x+r != x1 and x-r != x1+x2:
                        if prestep_y > y:
                             y1-=int(r/50*5)
-       if motion == U_L:
+        if motion == U_L:
            if x>x1+x2:
                if  prestep_y > y:
                    if y+r != y1 and y-r != y1+y2:
@@ -99,39 +103,122 @@ while Play:
                        if prestep_y > y:
                             y1-=int(r/50*5)
     if motion == LEFT :
-        prestep_x = x
-        x -= int(r/50*5)
+        if a ==1:
+            prestep_x = x
+            x -= int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_x = x
+            x -= int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_x = x
+            x -= int(5*50/r)
+        a = 0
     if motion == RIGHT :
-        prestep_x = x
-        x += int(r/50*5)
+        if a ==1:
+            prestep_x = x
+            x += int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_x = x
+            x += int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_x = x
+            x += int(5*50/r)
+        a = 0
     if motion == UP :
-       prestep_y = y
-       y -= int(r/50*5)
+        if a ==1:
+            prestep_y = y
+            y -= int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_y = y
+            y -= int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_y = y
+            y -= int(5*50/r)
+        a = 0
     if motion == DOWN :
-        prestep_y = y
-        y += int(r/50*5)
+        if a ==1:
+            prestep_y = y
+            y += int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_y = y
+            y += int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_y = y
+            y += int(5*50/r)
+        a = 0
     if motion == D_R :
-        prestep_x = x
-        prestep_y = y
-        y += int(r/50*5)
-        x += int(r/50*5)
+        if a ==1:
+            prestep_x = x
+            prestep_y = y
+            y += int(r/50*5)
+            x += int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y += int(5*50/r)
+            x += int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y += int(5*50/r)
+            x += int(5*50/r)
+        a = 0
+            
     if motion == D_L :
-        prestep_x = x
-        prestep_y = y
-        y += int(r/50*5)
-        x -= int(r/50*5)
+        if a ==1:
+            prestep_x = x
+            prestep_y = y
+            y += int(r/50*5)
+            x -= int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y += int(5*50/r)
+            x -= int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y += int(5*50/r)
+            x -= int(5*50/r)
+        a = 0
+        
+            
     if motion == U_R :
-        prestep_x = x
-        prestep_y = y
-        y -= int(r/50*5)
-        x += int(r/50*5)
+        if a ==1:
+            prestep_x = x
+            prestep_y = y
+            y -= int(r/50*5)
+            x += int(r/50*5)
+        if r >= 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y -= int(5*50/r)
+            x += int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y -= int(5*50/r)
+            x += int(5*50/r)
+        a = 0
     if motion == U_L :
-        prestep_x = x
-        prestep_y = y
-        y -= int(r/50*5)
-        x -= int(r/50*5)
+        if a ==1:
+            prestep_x = x
+            prestep_y = y
+            y -= int(r/50*5)
+            x -= int(r/50*5)
+        if r >= 50 and a == 0:
+            prestep_x = x
+            prestep_y = y
+            y -= int(5*50/r)
+            x -= int(5*50/r)
+        if r < 50 and a ==0:
+            prestep_x = x
+            prestep_y = y
+            y -= int(5*50/r)
+            x -= int(5*50/r)
+        a = 0
     if motion == rad_p :
-        r+=int(r/50*5)
+        r+=1
     if motion == rad_m :
-        r-=int(r/50*5)
+        r-=1
     clock.tick(FPS)
