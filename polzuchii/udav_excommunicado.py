054475293b464f0fbb0345cf545cf15c67ea875a
aid219 <aid219@DESKTOP-H9BSPJU>
2019-06-18 16:27:20 +0300

    14

--- polzuchii/udav_excommunicado.py
+++ polzuchii/udav_excommunicado.py
@@ -12,38 +12,26 @@ sc = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
 # координаты и радиус круга
 x = 400
-s=0
+s,iz,a,w,q=0,0,0,0,0
 y = 300
 Play = True
 sc.fill(white)
 roll = False
 direction = 1
 speed =3 # задаешь удаву ускорение!
-size = 10  # задаешь размеры ползучей сучары
+size = 30  # задаешь размеры ползучей сучары
 luck =200
-
-
-
-
-
-
 # задаешь как далеко он сможет проползти в одном направлении, если повезет конечно
-color = (0,0,0)
-dep_x = 0
-dep_y = 0
-iz = 0
-sp_xy =[]
-a = 0
-sp_yx = []
-w = 0
-q = 0 
-sp_main = []
-      
-   
+sp_y,sp_xy,sp_x,sp_yx,sp_main,sp_main2 =[],[],[],[],[],[]
+spiss = [1,2,3,4]
+
 while Play:
-    sp_yx=[x,y]
-    if sp_yx in sp_main:
-            
+    
+    rnd = random.randint(0,luck)  
+    sp_xy=[x,y]
+    sp_yx=[x+size,y+size]
+ 
+    if sp_xy in sp_main:
         if direction == 1:
             x -=speed
             direction = random.choice([2,3,4])
@@ -59,16 +47,11 @@ while Play:
         elif direction == 4:
             y +=speed
             direction = random.choice([1,2,3])
-                
-    
     if s == 0 :
-        
-        step2 =  y + random.randint(0,luck)
-        step1 = x + random.randint(0,luck)
-        step3 = x - random.randint(0,luck)
-        step4 = y - random.randint(0,luck)
+        step1,step2 =  x+ rnd,y+ rnd
+        step3,step4 = x- rnd,y- rnd
         a = 1
-    pygame.draw.rect(sc, color, (x,y,size,size))
+    pygame.draw.rect(sc, black, (x,y,size,size))
     pygame.display.update()
     for i in pygame.event.get():
        if i.type == pygame.QUIT:
@@ -76,8 +59,11 @@ while Play:
        elif i.type == pygame.KEYDOWN:
         if i.key == pygame.K_LEFT:
             pass
-    sp_xy = [x,y]
-    sp_main.append(sp_xy)
+    
+    sp_x = [x,y]
+    sp_y = [x+size,y+size]
+    sp_main.append(sp_x)
+    sp_main2.append(sp_y)
     if a ==1:
         direction = random.choice([1,2,3,4])
         s = 1
@@ -94,9 +80,6 @@ while Play:
     if y < +size*2:
         direction = random.choice([1,2,3])
         s = 1
-
-    
-
     if direction == 1 :
         if x < step1:
             x+=speed
@@ -117,7 +100,4 @@ while Play:
             y-=speed
         else:
             s =0
-     
-        
-  
     clock.tick(FPS)
