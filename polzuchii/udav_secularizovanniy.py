ab37d4fa50fc6ede235860a2b26570b40dde25aa
Ярослав <@DESKTOP-9DD6V4F>
2019-06-18 16:20:19 +0300

    noviyproriv

--- polzuchii/udav_secularizovanniy.py
+++ polzuchii/udav_secularizovanniy.py
@@ -1,13 +1,13 @@
-
+# ver 0.1
 import pygame
 import time 
 import random
 
 
+
 # Параметры наблюдательного, о кошка!
 FPS = 60
 Resolution = 1200,600  # размер экрана
-
 
 colors = {'black': (0,0,0),
            'white':(255,255,255),
@@ -23,23 +23,70 @@ clock = pygame.time.Clock()
 
 
 
-#Параметры этой гадюки
-speed = 3 # задаешь удаву ускорение!
-size = 10  # задаешь размеры ползучей сучары
+class Udav():
+    def __init__(self, name):
+        self.name = name
+        
+        #Параметры этой гадюки
+        self.speed = 3 # задаешь удаву ускорение!
+        self.size = 10  # задаешь размеры ползучей сучары
 
-x,y = 400,300
+        self.direction = ''
+        self.random_direct()
+        self.position = 400,300
+
+        self.allcoord = []
+
+    def random_direct(self):
+        directions = ['left','right','up','down']
+        self.direction = random.choice(directions)
+         
+    def set_coord_to_draw(self):
+        x,y = self.position
+        return  x - self.size, y - self.size, self.size * 2, self.size * 2
+    
+    def movement(self):
+
+        global Resolution
+
+        prestep = self.position
+
+        x,y = self.position
+        if self.direction == 'left':
+            x += round(self.size / 2)
+        if self.direction == 'right':
+            x -= round(self.size / 2)
+        if self.direction == 'up':
+            y += round(self.size / 2)
+        if self.direction == 'down':
+            y -= round(self.size / 2)
+        self.position = x,y
+
+        print(self.position)
+
+        if self.position > Resolution or self.position < (0,0):
+            self.position = prestep
+        
+        
+
+    def collision(self):
+        precoord = self.position
+   
+    def draw_rect(self,screen,color):
+        
+        pygame.draw.rect(screen,color,self.set_coord_to_draw())
 
 
-sc.fill(colors['white']) #Замалафили экран  ъУъ СУКА!
 
+#Создание шакалов капашащихся
+sc.fill(colors['white']) #Замалафили экран ъУъ СУКА!
+udav = Udav('udavsk')
 
 Play = True # Разрешение на ползучесть
 
+while Play:
+    udav.movement()
+    udav.draw_rect(sc,colors['black'])
 
-# ФункциАНАЛ
-def set_coord(x,y,size):
-    return  x-size, y-size, size*2, size*2
-
-
-def collision(x,y,size):
-    pass
+    udav.random_direct()
+    pygame.display.update()
