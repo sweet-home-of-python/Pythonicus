0c1cbb9e06fa2def6b1ea541634decff162f90e7
Ярослав <@DESKTOP-9DD6V4F>
2019-06-19 12:56:39 +0300

    ebuchiy udfv rabotaet

--- polzuchii/udav_secularizovanniy.py
+++ polzuchii/udav_secularizovanniy.py
@@ -16,6 +16,7 @@ colors = {'black': (0,0,0),
            'blue':(0,0,255)}
 
 
+
 #Инициализация длиннохвостого ублюдка
 pygame.init()
 sc = pygame.display.set_mode(Resolution)
@@ -28,7 +29,7 @@ class Udav():
         self.name = name
         
         #Параметры этой гадюки
-        self.speed = 3 # задаешь удаву ускорение!
+        self.speed = 3 # задаешь удаву ускорение! (нет)
         self.size = 10  # задаешь размеры ползучей сучары
 
         self.direction = ''
@@ -38,39 +39,55 @@ class Udav():
         self.allcoord = []
 
     def random_direct(self):
+        '''Возвращает случаюное направление'''
         directions = ['left','right','up','down']
         self.direction = random.choice(directions)
          
     def set_coord_to_draw(self):
+        '''Преобразует центральные координаты в координаты для квадрата'''
         x,y = self.position
         return  x - self.size, y - self.size, self.size * 2, self.size * 2
     
     def movement(self):
 
-        global Resolution
-
         prestep = self.position
 
         x,y = self.position
         if self.direction == 'left':
-            x += round(self.size / 2)
+            x += 2 * self.size
         if self.direction == 'right':
-            x -= round(self.size / 2)
+            x -= 2 * self.size
         if self.direction == 'up':
-            y += round(self.size / 2)
+            y += 2 * self.size
         if self.direction == 'down':
-            y -= round(self.size / 2)
-        self.position = x,y
-
-        print(self.position)
-
-        if self.position > Resolution or self.position < (0,0):
-            self.position = prestep
+            y -= 2 * self.size
+           
         
+        self.position = x,y
+        
+        if self.collision():
+            self.position = prestep
+
+        # Проверяем нет ли уже такой координаты, чтоб не повторяться
+        if self.position not in self.allcoord:
+            self.allcoord.append(self.position)
+        
+        
+        #print(self.position)
+
         
 
     def collision(self):
-        precoord = self.position
+        x,y = self.position
+        global Resolution
+        sx,sy = Resolution
+        if self.position in self.allcoord:
+            return True
+        #if (x+self.size,y+self.size) in self.allcoord or (x-self.size,y-self.size) in self.allcoord:
+           # return True
+        elif x >= sx or x <= 0 or y >= sy or y <= 0:
+            return True
+        else: return False
    
     def draw_rect(self,screen,color):
         
@@ -83,10 +100,16 @@ sc.fill(colors['white']) #Замалафили экран ъУъ СУКА!
 udav = Udav('udavsk')
 
 Play = True # Разрешение на ползучесть
-
+i = random.randint(0,3)
 while Play:
     udav.movement()
+    rand = random.randint(0,255)
     udav.draw_rect(sc,colors['black'])
+    if i <= 0:
+        udav.random_direct()
+        i = random.randint(0,3)
+    i -= 1
 
-    udav.random_direct()
+    
+
     pygame.display.update()
